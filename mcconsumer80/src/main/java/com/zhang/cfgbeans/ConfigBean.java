eb5c39bd9eb9f7b6dc3c2556ddd8f128cf569b5c
zhangwei <664886892@qq.com>
2019-03-23 23:14:12 +0800

    切换负载平衡策略

--- mcconsumer80/src/main/java/com/zhang/cfgbeans/ConfigBean.java
+++ mcconsumer80/src/main/java/com/zhang/cfgbeans/ConfigBean.java
@@ -1,6 +1,8 @@
 package com.zhang.cfgbeans;
 
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import com.netflix.loadbalancer.IRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,5 +13,9 @@ public class ConfigBean {
     @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
+    }
+    @Bean
+    public IRule myRule(){
+        return new RandomRule();
     }
 }
